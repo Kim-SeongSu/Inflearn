5059a2d41b0d05c446de712b732501372082c020
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-11-20 00:49:44 +0900

    Update: sol. TestCalendarPrinter
    달력만들기 문제 풀이 업로드 (완료)

--- 03. 자바 중급 1편/src/time/test/TestCalendarPrinter.java
+++ 03. 자바 중급 1편/src/time/test/TestCalendarPrinter.java
@@ -21,12 +21,72 @@ public class TestCalendarPrinter {
         System.out.print("월을 입력하세요: ");
         int month = sc.nextInt();
 
+        PrintCalendar(year, month);
+    }
+
+    private static void PrintCalendar (int year, int month) {
         System.out.println("Su Mo Tu We Th Fr Sa");
 
         LocalDate firstDay = LocalDate.of(year, month, 1);
-        DayOfWeek weekOfFirstDay = firstDay.getDayOfWeek();
+        LocalDate lastDay = LocalDate.of(year, month, firstDay.lengthOfMonth());
 
-        System.out.println(weekOfFirstDay);
+        // 첫째주 월요일 전까지 빈칸 생성
+        int offsetFirstDay = firstDay.getDayOfWeek().getValue() % 7;
+        for (int i=0; i < offsetFirstDay; i++) {
+            System.out.print("   ");
+        }
 
+        LocalDate currentDay = firstDay;
+        while (!currentDay.equals(lastDay.plusDays(1))) {
+            System.out.printf("%2d ",currentDay.getDayOfMonth());
+            if (currentDay.getDayOfWeek() == DayOfWeek.SATURDAY) System.out.println();
+            currentDay = currentDay.plusDays(1);
+        }
     }
 }
+
+/*
+# 정답
+package time.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TestCalendarPrinter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("년도를 입력하세요: ");
+        int year = scanner.nextInt();
+
+        System.out.print("월을 입력하세요: ");
+        int month = scanner.nextInt();
+
+        printCalendar(year, month);
+    }
+
+    public static void printCalendar(int year, int month) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        LocalDate firstDayOfNextMonth = firstDayOfMonth.plusMonths(1);
+
+        //월요일=1(1%7=1), ... 일요일=7(7%7=0)
+        int offsetWeekDays = firstDayOfMonth.getDayOfWeek().getValue() % 7;
+
+        System.out.println("Su Mo Tu We Th Fr Sa ");
+
+        for (int i = 0; i < offsetWeekDays; i++) {
+            System.out.print(" ");
+        }
+
+        LocalDate dayIterator = firstDayOfMonth;
+        while (dayIterator.isBefore(firstDayOfNextMonth)) {
+            System.out.printf("%2d ", dayIterator.getDayOfMonth());
+            if (dayIterator.getDayOfWeek() == DayOfWeek.SATURDAY) {
+                System.out.println();
+            }
+            dayIterator = dayIterator.plusDays(1);
+        }
+    }
+}
+ */
