967e37f87f0023f21ff4d960868f4fa1065c0440
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2025-01-01 23:32:52 +0900

    Upload: new practice directory
    불필요한 파일 제거 후, 실습 파일만 업로드

---
+++ 05. 스프링 입문/practice/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package hello.hello_spring.repository;
+
+
+import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+// implements MemberRepository까지 입력 후, alt + enter로 implement method 불러오기
+//@Repository   //직접 스프링 빈 등록을 위해 주석처리
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
