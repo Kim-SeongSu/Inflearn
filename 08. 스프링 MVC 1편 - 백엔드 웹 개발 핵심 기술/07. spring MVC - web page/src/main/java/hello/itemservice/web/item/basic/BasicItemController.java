1b05c7ec1661f62efbe411b0e43d62f60d61262d
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2025-03-02 22:21:05 +0900

    Upload: 07. spring MVC - web page
    7. 스프링 MVC  - 웹 페이지 만들기
     - 상품 등록, 수정 (@ModelAttribute)

--- 08. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/07. spring MVC - web page/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ 08. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/07. spring MVC - web page/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +31,82 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    // 상품 등록 Form (add) - thymeleaf
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    // 상품 등록 처리 Ver.1 (@RequestParam 사용) - thymeleaf
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 Ver.2 (@ModelAttribute 사용) - thymeleaf
+    /**
+     * model.addAttribute("item", item); 자동 추가
+     */
+    //@PostMapping("/add")
+    //public String addItemV2(@ModelAttribute("item") Item item , Model model) {
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가 (생략 가능)
+
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 Ver.3 (@ModelAttribute 이름 생략) - thymeleaf
+    /**
+     *  @ModelAttribute name 생략 가능
+     *  생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 Ver.4 (@ModelAttribute 자체 생략) - thymeleaf
+    /**
+     *  @ModelAttribute  자체 생략 가능
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+    // 상품 수정 Form
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    // 상품 수정 처리 (리다이렉트)
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,
+                       @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
